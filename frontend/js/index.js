9f033e73c4f87db733a22b16676af2a4f310e782
Hindu2737 <hinduguntreddi27@gmail.com>
2026-06-20 07:52:00 +0530

    toggle is added

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,33 @@
 //alert("Hello from JS!!");
 
-console.log("Hello from JS!!");
+//console.log("Hello from JS!!");
+
+const users = [
+    {
+        name: "John Doe",
+        gender: "Male",
+        image: "../images/john autouggest.png"
+    },
+    {
+        name: "Jenny Smith",
+        gender: "Female",
+        image: "../images/jhon wife autosuggest.webp"
+    }
+];
+
+let currentUser = 0;
+
+const btn = document.getElementById("toggleBtn");
+const nameEl = document.getElementById("userName");
+const genderEl = document.getElementById("userGender");
+const imageEl = document.getElementById("userImage");
+
+if (btn) {
+    btn.addEventListener("click", () => {
+        currentUser = (currentUser + 1) % users.length;
+
+        nameEl.textContent = users[currentUser].name;
+        genderEl.textContent = users[currentUser].gender;
+        imageEl.src = users[currentUser].image;
+    });
+}
